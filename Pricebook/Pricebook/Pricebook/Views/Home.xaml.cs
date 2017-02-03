a7a4c10766b7e747e6b7d7797b63edabc3962bfd
Aaron Campf <aaroncampf@gmail.com>
2017-02-03 14:36:57 -0800

    Added Project

---
+++ Pricebook/Pricebook/Pricebook/Views/Home.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Pricebook.Views {
+  public partial class Home : ContentPage {
+    public string MainText { get; set; } = "Hello World";
+
+    public Home() {
+      InitializeComponent();
+      this.BindingContext = this;
+    }
+  }
+}
