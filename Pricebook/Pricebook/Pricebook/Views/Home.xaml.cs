edc9c910cec02de5318960a40b7316c18270f0f0
Aaron Campf <aaroncampf@gmail.com>
2017-02-06 18:03:28 -0800

    More work

--- Pricebook/Pricebook/Pricebook/Views/Home.xaml.cs
+++ Pricebook/Pricebook/Pricebook/Views/Home.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,25 +10,33 @@ using Xamarin.Forms;
 namespace Pricebook.Views {
   public partial class Home : ContentPage {
     public string MainText { get; set; } = "Hello World";
-    public List<Database.INVMAS> INVMAS_Table { get; private set; }
+    //public List<Database.INVMAS> INVMAS_Table { get; private set; 
+    public ObservableCollection<Database.INVMAS> INVMAS_Table { get; private set; } = new ObservableCollection<Database.INVMAS>();
 
 
     public Home() {
       InitializeComponent();
       this.BindingContext = this;
-
-
+      Xamarin.Forms.DataGrid.DataGridComponent.Init();
 
       var CSV_INVMAS = Pricebook.Properties.Resources.INVMAS;
-      var Test = new System.IO.StringReader(CSV_INVMAS);
-      INVMAS_Table = new CsvHelper.CsvReader(Test).GetRecords<Database.INVMAS>().ToList();
+      foreach (var item in new CsvHelper.CsvReader(new System.IO.StringReader(CSV_INVMAS)).GetRecords<Database.INVMAS>()) {
+        INVMAS_Table.Add(item);
+      }
 
-      //dgdCustomer.Children.Add(new Label() { Text = "111111111111111111111111111111" });
-      //dgdCustomer.Children.Add(new Label() { Text = "222222222222222222222222222222" }, 2, 3, 1, 3);
+      /*
+      foreach (var item in INVMAS_Table.Take(INVMAS_Table.Count - 10).ToList()) {
+        INVMAS_Table.Remove(item);
+      }
+      */
+    }
 
-      listView.ItemsSource = INVMAS_Table;
+    private void GridTest_ItemSelected(object sender, SelectedItemChangedEventArgs e) {
+      Navigation.PushAsync(new ProductPage() { SelectedProduct = (Database.INVMAS)e.SelectedItem });
+    }
 
-
+    private void GridTest_Refreshing(object sender, EventArgs e) {
+      throw new NotImplementedException();
     }
   }
 }
---
+++ Pricebook/Pricebook/Pricebook/Views/ProductPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Pricebook.Views {
+  public partial class ProductPage : ContentPage {
+    public Database.INVMAS SelectedProduct { get; set; }
+
+    public ProductPage() {
+      InitializeComponent();
+      this.BindingContext = this;
+    }
+  }
+}
