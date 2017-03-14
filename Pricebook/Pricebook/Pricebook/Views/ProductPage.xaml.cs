f9920ae733220cbedc789c1f12cbfb6d65c07962
Aaron Campf <aaroncampf@gmail.com>
2017-03-14 12:42:30 -0700

    Optionally showed items in the Product

--- Pricebook/Pricebook/Pricebook/Views/ProductPage.xaml.cs
+++ Pricebook/Pricebook/Pricebook/Views/ProductPage.xaml.cs
@@ -22,40 +22,43 @@ namespace Pricebook.Views {
                           <p>CATG: {SelectedProduct.CATG}</p>
                           <p>GROUP: {SelectedProduct.GROUP}</p>
                           <p>SUPERGROUP: {SelectedProduct.SUPERGROUP}</p>
-                          <p>DESCRIP: {SelectedProduct.DESCRIP}</p>
-                          <p>DESCRIP2: {SelectedProduct.DESCRIP2}</p>
-                          <p>DESCRIP3: {SelectedProduct.DESCRIP3}</p>
+                          <p>DESCRIP: {SelectedProduct.DESCRIP}</p>";
+
+      if (!string.IsNullOrEmpty(SelectedProduct.DESCRIP2))
+        htmlSource.Html += $"<p>DESCRIP2: {SelectedProduct.DESCRIP2}</p>";
+      if (!string.IsNullOrEmpty(SelectedProduct.DESCRIP3))
+        htmlSource.Html += $"<p>DESCRIP3: {SelectedProduct.DESCRIP3}</p>";
+
+      htmlSource.Html += $@"
                           <p>LINECOST: {SelectedProduct.LINECOST}</p>
                           <p>DEADNET: {SelectedProduct.DEADNET}</p>
                           <p>LOADEDCOST: {SelectedProduct.LOADEDCOST}</p>
                           <p>VENDORID: {SelectedProduct.VENDORID}</p>
-                          <p>DELIV_DAYS: {SelectedProduct.DELIV_DAYS}</p>
-                          <p>DAYS_OH: {SelectedProduct.DAYS_OH}</p>
-                          <p>LASTDATE: {SelectedProduct.LASTDATE:D}</p>
                           <p>Location: {SelectedProduct.WHSE_LOC} - {SelectedProduct.WHSE_BIN}</p>
-                          <p>SELLUOM: {SelectedProduct.SELLUOM}</p>
-                          <p>BUY_UOM: {SelectedProduct.BUY_UOM}</p>
-                          <p>PRBK_SEQ: {SelectedProduct.PRBK_SEQ}</p>
-                          <p>SELL_CALC1: {SelectedProduct.SELL_CALC1}</p>
-                          <p>ITEMNO: {SelectedProduct.SELL_CALC1}</p>
-                          <p>SELL_CALC2: {SelectedProduct.SELL_CALC2}</p>
-                          <p>SELL_CALC3: {SelectedProduct.SELL_CALC3}</p>
-                          <p>SELL_CALC4: {SelectedProduct.SELL_CALC4}</p>
-                          <p>SELL_CALC5: {SelectedProduct.SELL_CALC5}</p>
-                          <p>SELL_CALC6: {SelectedProduct.SELL_CALC6}</p>
-                          <p>SELL_CALC7: {SelectedProduct.SELL_CALC7}</p>
-                          <p>SELL_CALC8: {SelectedProduct.SELL_CALC8}</p>
-                          <p>SELL_CALC9: {SelectedProduct.SELL_CALC9}</p>
-
-                          <p>...</p>
-                          <h2>Special Prices</h2>
-";
+                          <p>SELLUOM: {SelectedProduct.SELLUOM}</p>";
 
 
+      if (!string.IsNullOrEmpty(SelectedProduct.SELL_CALC1))
+        htmlSource.Html += $"<p>SELL_CALC1: {SelectedProduct.SELL_CALC1}</p>";
+      if (!string.IsNullOrEmpty(SelectedProduct.SELL_CALC2))
+        htmlSource.Html += $"<p>SELL_CALC2: {SelectedProduct.SELL_CALC2}</p>";
+      if (!string.IsNullOrEmpty(SelectedProduct.SELL_CALC3))
+        htmlSource.Html += $"<p>SELL_CALC3: {SelectedProduct.SELL_CALC3}</p>";
+      if (!string.IsNullOrEmpty(SelectedProduct.SELL_CALC4))
+        htmlSource.Html += $"<p>SELL_CALC4: {SelectedProduct.SELL_CALC4}</p>";
+      if (!string.IsNullOrEmpty(SelectedProduct.SELL_CALC5))
+        htmlSource.Html += $"<p>SELL_CALC5: {SelectedProduct.SELL_CALC5}</p>";
+      if (!string.IsNullOrEmpty(SelectedProduct.SELL_CALC6))
+        htmlSource.Html += $"<p>SELL_CALC6: {SelectedProduct.SELL_CALC6}</p>";
+      if (!string.IsNullOrEmpty(SelectedProduct.SELL_CALC7))
+        htmlSource.Html += $"<p>SELL_CALC7: {SelectedProduct.SELL_CALC7}</p>";
+      if (!string.IsNullOrEmpty(SelectedProduct.SELL_CALC8))
+        htmlSource.Html += $"<p>SELL_CALC8: {SelectedProduct.SELL_CALC8}</p>";
+      if (!string.IsNullOrEmpty(SelectedProduct.SELL_CALC9))
+        htmlSource.Html += $"<p>SELL_CALC9: {SelectedProduct.SELL_CALC9}</p>";      
+
+      htmlSource.Html += "<h2>Special Prices</h2>";
       webMain.Source = htmlSource;
     }
-
-
-
   }
 }
